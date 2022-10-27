5136e4881194aab9795c5d266fe16d01cb9712fd
Floris <kappenfloris@gmail.com>
2022-10-27 19:43:35 +0200

    feature: add timer functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'package:figure_drawing/pages/home.dart';
 
 void main() {
--- lib/pages/display.dart
+++ lib/pages/display.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 import 'dart:io' as io;
+import 'dart:async' as async;
+
+import 'package:figure_drawing/classes.dart' as classes;
 
 class DisplayPage extends StatefulWidget {
   final List<String> imagePaths;
+  final classes.UserSettings userSettings;
 
-  const DisplayPage({ required this.imagePaths });
+  const DisplayPage({
+    required this.imagePaths,
+    required this.userSettings
+  });
 
   @override
   _DisplayPageState createState() => _DisplayPageState();
@@ -12,6 +19,50 @@ class DisplayPage extends StatefulWidget {
 
 class _DisplayPageState extends State<DisplayPage> {
   int currentIndex = 0;
+  late async.Timer _timer;
+  int _start = 0;
+
+  void startTimer() {
+    const oneSec = Duration(seconds: 1);
+    _timer = async.Timer.periodic(
+      oneSec,
+          (async.Timer timer) {
+        if (_start == 0) {
+          if (currentIndex < widget.imagePaths.length - 1) {
+            setState(() {
+              currentIndex += 1;
+              _start = widget.userSettings.timeBetweenImages;
+            });
+          } else {
+            setState(() {
+              timer.cancel();
+            });
+          }
+        } else {
+          setState(() {
+            _start--;
+          });
+        }
+      },
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      setState(() {
+        _start = widget.userSettings.timeBetweenImages;
+      });
+      startTimer();
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,10 +74,19 @@ class _DisplayPageState extends State<DisplayPage> {
         child: Column (
           children: <Widget>[
             Expanded(
-              child:  Image.file(
-                io.File(widget.imagePaths[currentIndex]),
-                fit: BoxFit.contain,
-              )
+                child: Stack(
+                  children: <Widget>[
+                    Image.file(
+                      io.File(widget.imagePaths[currentIndex]),
+                      fit: BoxFit.contain,
+                    ),
+                    Positioned(
+                      top: 12,
+                      right: 12,
+                      child: Text("$_start")
+                    )
+                  ],
+                ),
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
