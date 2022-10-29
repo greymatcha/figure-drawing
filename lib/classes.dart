39879a5551e9d983d2b141ea7829513db4f2d6f2
Floris <kappenfloris@gmail.com>
2022-10-29 13:25:04 +0200

    feature: build base session scheduler

--- lib/classes.dart
+++ lib/classes.dart
@@ -1,5 +1,23 @@
+import 'package:flutter/material.dart';
+
 class UserSettings {
   int timeBetweenImages;
 
   UserSettings(this.timeBetweenImages);
+}
+
+enum SessionItemType { pause, draw }
+
+class SessionItem {
+  Key key;
+  SessionItemType type;
+
+  // For a break, this decides how long the break is. For an image it decides
+  // how much time is spent per image.
+  int? timeAmount;
+
+  // Optional
+  int? imageAmount;
+
+  SessionItem(this.key, this.type, this.timeAmount, this.imageAmount);
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:file_picker/file_picker.dart';
 
 import 'package:figure_drawing/pages/display.dart';
+import 'package:figure_drawing/pages/session_item.dart';
 import 'package:figure_drawing/utilities/utilities.dart' as utilities;
 import 'package:figure_drawing/classes.dart' as classes;
 
@@ -25,16 +26,14 @@ class _HomePageState extends State<HomePage> {
   List<String> imagePaths = [];
   String folderName = "";
   bool hasSelected = false;
+  int sessionKey = 0;
 
   int timerValue = 30;
-  List<Object> sessionData = [
-    {
-      "key": "dawdaw",
-      "type": "break",
-      "isBreak": false,
-      "imageAmount": 10,
-      "timePerImage": 30
-    }
+  List<classes.SessionItem> session = [
+    classes.SessionItem(const Key("hi"), classes.SessionItemType.draw, 12, 12),
+    classes.SessionItem(const Key("hi1"), classes.SessionItemType.draw, 5, 5),
+    classes.SessionItem(const Key("hi2"), classes.SessionItemType.pause, 120, null),
+    classes.SessionItem(const Key("hi3"), classes.SessionItemType.draw, 8, 8)
   ];
 
   @override
@@ -68,78 +67,76 @@ class _HomePageState extends State<HomePage> {
         ),
         body: TabBarView(
             children: [
-              Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    hasSelected ?
+              Column(
+                children: <Widget>[
+                  const SizedBox(height: 24),
+                  hasSelected ?
                     Text('Found ${imagePaths.length.toString()} images in "$folderName"') :
                     const Text("Supported image types: jpg, png, webp, gif"),
-                    const SizedBox(height: 10),
-                    ElevatedButton(
-                      child: Text((() {
-                        if (hasSelected) {
-                          return 'Select a different folder';
-                        } else {
-                          return 'Select a folder';
-                        }
-                      }())),
-                      onPressed: () async {
-                        String? selectedDirectory = await FilePicker.platform.getDirectoryPath();
-                        if (selectedDirectory != null) {
-                          setState(() {
-                            imagePaths = utilities.getImagesInDirectoryRecursive(selectedDirectory);
-                            folderName = selectedDirectory;
-                            hasSelected = true;
-                          });
-                        }
-                      },
-                    ),
-                    Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          const Text('Time per image:'),
-                          const SizedBox(width: 10),
-                          DropdownButton(
-                            value: timerValue,
-                            icon: const Icon(Icons.arrow_downward),
-                            elevation: 16,
-                            onChanged: (Object? selectedItem) {
-                              setState(() {
-                                timerValue = selectedItem as int;
-                              });
-                            },
-                            items: timerDropdownOptions.map((Map item) {
-                              return DropdownMenuItem(
-                                  value: item["value"],
-                                  child: Text(item["label"])
-                              );
-                            }).toList(),
-                          ),
-                        ]
-                    ),
-                    Column(
+                  const SizedBox(height: 10),
+                  ElevatedButton(
+                    child: Text((() {
+                      if (hasSelected) {
+                        return 'Select a different folder';
+                      } else {
+                        return 'Select a folder';
+                      }
+                    }())),
+                    onPressed: () async {
+                      String? selectedDirectory = await FilePicker.platform.getDirectoryPath();
+                      if (selectedDirectory != null) {
+                        setState(() {
+                          imagePaths = utilities.getImagesInDirectoryRecursive(selectedDirectory);
+                          folderName = selectedDirectory;
+                          hasSelected = true;
+                        });
+                      }
+                    },
+                  ),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
-                        const SizedBox(height: 10),
-                        ElevatedButton(
-                            child: const Text('Start'),
-                            onPressed: () {
-                              if (hasSelected) {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(builder: (context) => DisplayPage(
-                                    imagePaths: imagePaths,
-                                    userSettings: classes.UserSettings(timerValue),
-                                  )),
-                                );
-                              }
+                        const Text('Time per image:'),
+                        const SizedBox(width: 10),
+                        DropdownButton(
+                          value: timerValue,
+                          icon: const Icon(Icons.arrow_downward),
+                          elevation: 16,
+                          onChanged: (Object? selectedItem) {
+                            setState(() {
+                              timerValue = selectedItem as int;
+                            });
+                          },
+                          items: timerDropdownOptions.map((Map item) {
+                            return DropdownMenuItem(
+                                value: item["value"],
+                                child: Text(item["label"])
+                            );
+                          }).toList(),
+                        ),
+                      ]
+                  ),
+                  Column(
+                    children: <Widget>[
+                      const SizedBox(height: 10),
+                      ElevatedButton(
+                          child: const Text('Start'),
+                          onPressed: () {
+                            if (hasSelected) {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) => DisplayPage(
+                                  imagePaths: imagePaths,
+                                  userSettings: classes.UserSettings(timerValue),
+                                )),
+                              );
                             }
-                        )
-                      ],
-                    )
-                  ],
-                )
-            ),
+                          }
+                      )
+                    ],
+                  )
+                ],
+              ),
               Center(
                 child: Column(
                   children: [
@@ -150,11 +147,26 @@ class _HomePageState extends State<HomePage> {
                         padding: const EdgeInsets.symmetric(horizontal: 40),
                         footer: Column(
                           children: [
+                            SizedBox(height: 12),
                             ElevatedButton(
-                              onPressed: () {},
+                              onPressed: () {
+                                _navigateAddEditPage(
+                                  context,
+                                  classes.SessionItem(
+                                    Key((sessionKey + 1).toString()),
+                                    classes.SessionItemType.draw,
+                                    null,
+                                    null
+                                  ),
+                                  null
+                                );
+                                setState(() {
+                                  sessionKey += 1;
+                                });
+                              },
                               style: ElevatedButton.styleFrom(
                                 maximumSize: const Size(120, 500),
-                                padding: EdgeInsets.fromLTRB(4, 18, 4, 18),
+                                padding: const EdgeInsets.fromLTRB(4, 18, 4, 18),
                               ),
                               child: Row(
                                 mainAxisAlignment: MainAxisAlignment.center,
@@ -167,21 +179,45 @@ class _HomePageState extends State<HomePage> {
                             ),
                           ],
                         ),
-                        children: [
-                          ListTile(
-                            key: Key("1"),
-                            title: Text("hi"),
-                            trailing: Row(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                IconButton(onPressed: () {}, icon: Icon(Icons.edit)),
-                                IconButton(onPressed: () {}, icon: Icon(Icons.delete_outline)),
-                                SizedBox(width: 32),
-                              ],
-                            )
-                          )
-                        ],
-                        onReorder: (int oldIndex, int newIndex) {}
+                        children:
+                          session.asMap().entries.map((MapEntry entry) {
+                            int index = entry.key;
+                            classes.SessionItem sessionItem = entry.value;
+                            return ListTile(
+                              key: sessionItem.key,
+                              title: Text(sessionItemDescription(sessionItem)),
+                              trailing: Row(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  IconButton(onPressed: () {
+                                    _navigateAddEditPage(
+                                        context,
+                                        sessionItem,
+                                        index
+                                    );
+                                  }, icon: const Icon(Icons.edit)),
+                                  IconButton(onPressed: () {
+                                    setState(() {
+                                      session.removeAt(index);
+                                    });
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                      const SnackBar(content: Text("Session item removed"))
+                                    );
+                                  }, icon: const Icon(Icons.delete_outline)),
+                                  const SizedBox(width: 32),
+                                ],
+                              )
+                            );
+                          }).toList(),
+                        onReorder: (int oldIndex, int newIndex) {
+                          setState(() {
+                            if (oldIndex < newIndex) {
+                              newIndex -= 1;
+                            }
+                            final classes.SessionItem sessionItem = session.removeAt(oldIndex);
+                            session.insert(newIndex, sessionItem);
+                          });
+                        }
                       )
                     ),
                   ]
@@ -192,4 +228,38 @@ class _HomePageState extends State<HomePage> {
       )
     );
   }
+
+  Future<void> _navigateAddEditPage(BuildContext context, classes.SessionItem sessionItem, int? existingIndex) async {
+    final classes.SessionItem? resultSessionItem = await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => SessionItemPage(sessionItem: sessionItem))
+    );
+    if (resultSessionItem != null) {
+      setState(() {
+        if (existingIndex != null) {
+          session[existingIndex] = resultSessionItem;
+          ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text("Session item updated"))
+          );
+        } else {
+          session.add(resultSessionItem);
+          ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text("Session item added"))
+          );
+        }
+      });
+    }
+  }
+}
+
+String sessionItemDescription(classes.SessionItem sessionItem) {
+  String result = "";
+
+  if (sessionItem.type == classes.SessionItemType.pause) {
+    result = "${sessionItem.timeAmount}s break";
+  } else {
+    result = "${sessionItem.imageAmount} drawings of ${sessionItem.timeAmount}s each";
+  }
+
+  return result;
 }
